8cf126b1a51c66a752d43d8fa190eb85dbc218b2
waltech1 <walTech@protonmail.com>
2022-08-16 13:06:21 -0600

    Menus

--- main.py
+++ main.py
@@ -1,7 +1,120 @@
+from __future__ import annotations
+import abc
 import logging
 from argparse import ArgumentParser
 
 from myxmpp.send import SendMsgBot
+from myxmpp.accounts.user import User
+
+
+class Menu(abc.ABC):
+    @abc.abstractclassmethod
+    def next(self) -> Menu:
+        pass
+
+
+class WrongOptionMenu(Menu):
+    def next(self) -> Menu:
+        print("Invalid option...")
+        return MainMenu()
+
+
+class ExitMenu(Menu):
+    def next(self) -> Menu:
+        print("See you later...")
+        return None
+
+
+class MainMenu(Menu):
+    def next(self) -> Menu:
+        print(
+            '1) Registrar una nueva cuenta en el servidor ',
+            '2) Iniciar sesión con una cuenta ',
+            '3) Cerrar sesión con una cuenta ',
+            '4) Eliminar la cuenta del servidor',
+            '5) Salir',
+            sep='\n'
+        )
+        selection = input()
+
+        if selection == '1':
+            return None
+        elif selection == '2':
+            return None
+        elif selection == '3':
+            return None
+        elif selection == '4':
+            return None
+        elif selection == '5':
+            return ExitMenu()
+        else:
+            return WrongOptionMenu()
+
+
+class LogginMenu(Menu):
+    def next(self) -> Menu:
+        username = input("Username: ")
+        password = input("Password: ")
+
+        user = User(username, password)
+
+
+class LoggedMenu(Menu):
+    def next(self) -> Menu:
+        print(
+            '1) Mostrar todos los usuarios/contactos y su estado ',
+            '2) Agregar un usuario a los contactos ',
+            '3) Mostrar detalles de contacto de un usuario ',
+            '4) Comunicación 1 a 1 con cualquier usuario/contacto ',
+            '5) Participar en conversaciones grupales ',
+            '6) Definir mensaje de presencia ',
+            '7) Enviar/recibir notificaciones ',
+            '8) Enviar/recibir archivos',
+            '9) Regresar',
+            sep='\n'
+        )
+        selection = input()
+
+        if selection == '1':
+            return None
+        elif selection == '2':
+            return None
+        elif selection == '3':
+            return None
+        elif selection == '4':
+            return None
+        elif selection == '5':
+            return None
+        elif selection == '6':
+            return None
+        elif selection == '7':
+            return None
+        elif selection == '8':
+            return None
+        elif selection == '9':
+            return ExitMenu()
+        else:
+            return WrongOptionMenu()
+
+
+class Executer:
+    current_menu: Menu = MainMenu()
+
+    def execute(self):
+        next_menu = self.current_menu.next()
+        self.current_menu = next_menu
+
+        return self.current_menu
+
+
+def run():
+    entry_point = Executer()
+
+    while True:
+        next_menu = entry_point.execute()
+
+        if not next_menu:
+            break
 
 
 if __name__ == '__main__':
