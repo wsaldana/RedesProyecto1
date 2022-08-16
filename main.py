c2fb3ab38533ce028d418562aca9ae6e2c6d2f4d
waltech1 <walTech@protonmail.com>
2022-08-16 07:37:47 -0600

    Init modules

--- main.py
+++ main.py
@@ -1,56 +1,10 @@
 import logging
-
 from argparse import ArgumentParser
-import slixmpp
 
-
-class SendMsgBot(slixmpp.ClientXMPP):
-
-    """
-    A basic Slixmpp bot that will log in, send a message,
-    and then log out.
-    """
-
-    def __init__(self, jid, password, recipient, message):
-        slixmpp.ClientXMPP.__init__(self, jid, password)
-
-        # The message we wish to send, and the JID that
-        # will receive it.
-        self.recipient = recipient
-        self.msg = message
-
-        # The session_start event will be triggered when
-        # the bot establishes its connection with the server
-        # and the XML streams are ready for use. We want to
-        # listen for this event so that we we can initialize
-        # our roster.
-        self.add_event_handler("session_start", self.start)
-
-    async def start(self, event):
-        """
-        Process the session_start event.
-
-        Typical actions for the session_start event are
-        requesting the roster and broadcasting an initial
-        presence stanza.
-
-        Arguments:
-            event -- An empty dictionary. The session_start
-                     event does not provide any additional
-                     data.
-        """
-        self.send_presence()
-        await self.get_roster()
-
-        self.send_message(mto=self.recipient,
-                          mbody=self.msg,
-                          mtype='chat')
-
-        self.disconnect()
+from myxmpp.send import SendMsgBot
 
 
 if __name__ == '__main__':
-    # Setup the command line arguments.
     parser = ArgumentParser(description=SendMsgBot.__doc__)
 
     # Output verbosity options.
@@ -90,8 +44,10 @@ if __name__ == '__main__':
     # have interdependencies, the order in which you register them does
     # not matter.
     xmpp = SendMsgBot(args.jid, args.password, args.to, args.message)
-    xmpp.register_plugin('xep_0030') # Service Discovery
-    xmpp.register_plugin('xep_0199') # XMPP Ping
+    # Service Discovery
+    xmpp.register_plugin('xep_0030')
+    # XMPP Ping
+    xmpp.register_plugin('xep_0199')
 
     # Connect to the XMPP server and start processing XMPP stanzas.
     xmpp.connect()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="myxmpp",
+    version="0.1.0",
+    author="Walter Saldaña",
+    author_email="sal19897@uvg.edu.gt",
+    packages=find_packages(),
+)
